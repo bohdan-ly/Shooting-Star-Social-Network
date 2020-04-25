5a54c572d61a7d33beb8a86dc0ca73de4d8188b8
TwelveGEater <dronkfc@gmail.com>
2020-04-25 17:10:47 +0300

    Updates

---
+++ precache-manifest.3b335cc5b04edab9233aea8776ad720d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "562752104c340847d1be109675118e5f",
+    "url": "/Shooting-Star-Social-Network/index.html"
+  },
+  {
+    "revision": "e9f311f605a1f42c1008",
+    "url": "/Shooting-Star-Social-Network/static/css/3.b34ed16b.chunk.css"
+  },
+  {
+    "revision": "b1a048bcbcab8c14d5d2",
+    "url": "/Shooting-Star-Social-Network/static/css/4.0997d91d.chunk.css"
+  },
+  {
+    "revision": "9475931307688cbaee46",
+    "url": "/Shooting-Star-Social-Network/static/css/5.0ab0ac3b.chunk.css"
+  },
+  {
+    "revision": "bb3c753257e1ef41d961",
+    "url": "/Shooting-Star-Social-Network/static/css/main.6b4d6877.chunk.css"
+  },
+  {
+    "revision": "52f8106fbb1ef1c5525b",
+    "url": "/Shooting-Star-Social-Network/static/js/2.d396910c.chunk.js"
+  },
+  {
+    "revision": "e9f311f605a1f42c1008",
+    "url": "/Shooting-Star-Social-Network/static/js/3.05ecf35d.chunk.js"
+  },
+  {
+    "revision": "b1a048bcbcab8c14d5d2",
+    "url": "/Shooting-Star-Social-Network/static/js/4.e620ffa7.chunk.js"
+  },
+  {
+    "revision": "9475931307688cbaee46",
+    "url": "/Shooting-Star-Social-Network/static/js/5.2d06ac61.chunk.js"
+  },
+  {
+    "revision": "bb3c753257e1ef41d961",
+    "url": "/Shooting-Star-Social-Network/static/js/main.f89deee9.chunk.js"
+  },
+  {
+    "revision": "7fa3d04d8b90b6bccf64",
+    "url": "/Shooting-Star-Social-Network/static/js/runtime~main.b2edb333.js"
+  },
+  {
+    "revision": "1130a1d6e6aa2ace8db58f7f4d357fb6",
+    "url": "/Shooting-Star-Social-Network/static/media/Eclipse-1s-200px.1130a1d6.svg"
+  },
+  {
+    "revision": "213517bd2e23e1574dba763a4623b2be",
+    "url": "/Shooting-Star-Social-Network/static/media/c8d7b982d403d6025c9809c78fbbe6c2.213517bd.jpg"
+  },
+  {
+    "revision": "edb7dc23c7e6794440754001d45ff682",
+    "url": "/Shooting-Star-Social-Network/static/media/logo.edb7dc23.png"
+  }
+]);
